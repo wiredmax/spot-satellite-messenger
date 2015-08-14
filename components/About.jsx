d2a5889fc8b8a5ccd8122d5957c3afc3a652d0c1
Maxime Poulin <mpoulin@roux.ca>
2015-08-14 15:15:32 -0400

    Render using a template

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = React.createClass({
   render: function() {
     return (
-      <div>About</div>
+      <div>{typeof(window) === "undefined" ? "server" : "client"}</div>
     );
   }
 });
